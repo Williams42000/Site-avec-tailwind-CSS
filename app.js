400681f25ed6235732fcba6cabc9af2e5e8de358
wiily mk <williamsmk@live.fr>
2021-07-27 12:32:00 -0400

    Premier code

---
+++ app.js
@@ -0,0 +1,8 @@
+const menuResponsible = document.querySelector('.toggle-menu');
+const iconeToggle = document.querySelector('.icone-toggle');
+
+iconeToggle.addEventListener('click', () => {
+
+     menuResponsible.classList.toggle('hidden');
+
+})
